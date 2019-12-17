2b9f618e7df2dd7d04814c5c9309defcea0ba5ab
Artur Aralov <artur.aralov@gmail.com>
2019-12-17 14:13:43 +0200

    Implement simple homepage

--- monto/src/pages/Home.js
+++ monto/src/pages/Home.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import AppNav from "../AppNav";
 import Example from "./Example";
+import Image from "../images/tracker.png"
 
 class Main extends Component {
   render() {
     return (
       <div>
         <AppNav />
-        <Example />
+        <img src={Image} width={window.innerWidth} height={window.innerHeight - 60}/>
       </div>
     );
   }
